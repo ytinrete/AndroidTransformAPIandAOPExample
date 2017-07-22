38977b08157e18382858a3a20a95474c9e1c0a7d
lirui2015 <lirui2015@corp.netease.com>
2017-07-22 14:30:08 +0800

    code

---
+++ app/src/main/java/com/example/transformtest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.transformtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    hello();
+    hello2(111);
+    findViewById(R.id.btn).setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    Toast.makeText(this, "btn", Toast.LENGTH_SHORT).show();
+  }
+
+  private void hello() {
+    Log.d("xxxx", "hello");
+  }
+
+  private void hello2(int aaa) {
+    Log.d("xxxx", "hello2:" + aaa);
+  }
+
+
+}
---
+++ app/src/main/java/com/example/transformtest/Test.java
@@ -0,0 +1,21 @@
+package com.example.transformtest;
+
+import android.util.Log;
+
+/**
+ *
+ */
+
+public class Test {
+  public static void test1() {
+    Log.d("xxxxx", "AOP---test1 success");
+  }
+
+  public static void test2() {
+    Log.d("xxxxx", "AOP---test2 success: ");
+  }
+
+  public static void test3() {
+    Log.d("xxxxx", "AOP---test3 success");
+  }
+}
